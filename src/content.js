27fd920094b4369c493f9ea3b649bc4648fb94d4
Moshe Hayoun <yelotag@gmail.com>
2026-05-05 23:42:31 +0300

    release 1.0.1 fix memo

--- src/content.js
+++ src/content.js
@@ -67,7 +67,7 @@ export const content = {
             {name: "henne-five.vercel.app (בתהליך)", url: "https://henne-five.vercel.app/", category: "Evenements", image:"/images/henne.png"},
             {name: "CompBuilder.com (בתהליך)", url: "https://unsteadfast-dionna-poignantly.ngrok-free.dev", category: "System Modeling", image:"/images/CompBuilder.png"},
             {name: "asado-catering.vercel.app (בתהליך)", url: "https://asado-catering.vercel.app/", category: "Catering", image:"/images/asado.png"},
-            {name: "memo4unix.com", url: "https://memo4unix.com", category: "Technology", image:"/images/asado.png"}
+            {name: "memo4unix.com", url: "https://memo4unix.com", category: "Technology", image:"/images/memo.png"}
         ]
     },
     he: {
